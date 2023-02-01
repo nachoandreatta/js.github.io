24879e2a0846e8394e141bd0933091dee2a0cb78
nachoandreatta <nachoandreatta@gmail.com>
2023-02-01 18:22:35 -0300

    subida 1.2

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
 //inicio
-alert("Bienvenida/o a la calculadora de Paypal")
+bienvenido ()
 
-//body
-let seguir;
+let edad = parseInt(prompt("Ingrese su edad"))
+if (edad < 18){
+    alert("No sos mayor")
+} else {
+    //body
+    let seguir;
 
-while (seguir != "s") {
+    while (seguir != "s") {
     //ingreso el valor
-    let inicio = parseInt(prompt("Ingresar 1 para calcular las comisiones si quieres recibir o ingresa 2 para calcular las comisiones si quieres enviar" ))
+        let inicio = parseInt(prompt("Ingresar 1 para calcular las comisiones si quieres recibir o ingresa 2 para calcular las comisiones si quieres enviar" ))
     //calculadora para recibir
-        while (inicio == 1) {
-            let valorRecibo = parseInt(prompt("Ingresa cuando quieres recibir"))
-                var resultadoRecibo = (valorRecibo + ((valorRecibo*5.4)/100) + 0.30)
-                    alert("para recibir " + valorRecibo + " USD debes enviar " + resultadoRecibo + " USD");
-        break;
-    }
+            while (inicio == 1) {
+                let valorRecibo = parseInt(prompt("Ingresa cuando quieres recibir"))
+                    let resultadoRecibo = (valorRecibo + ((valorRecibo*5.4)/100) + 0.30)
+                        alert("para recibir " + valorRecibo + " USD debes enviar " + resultadoRecibo + " USD");
+            break;
+        }
     //calculadora para enviar
-        while (inicio == 2) {
-            let valorEnvio = parseInt(prompt("Ingresa cuando quieres enviar"))
-                var resultadoEnvio = (valorEnvio - ((valorEnvio*5.4)/100) - 0.30)
-                    alert("si envias " + valorEnvio + " USD recibiras " + resultadoEnvio + " USD")
-        break;
-    }
+            while (inicio == 2) {
+                let valorEnvio = parseInt(prompt("Ingresa cuando quieres enviar"))
+                    let resultadoEnvio = (valorEnvio - ((valorEnvio*5.4)/100) - 0.30)
+                        alert("si envias " + valorEnvio + " USD recibiras " + resultadoEnvio + " USD")
+            break;
+        }
     //error al ingresar
-        while (inicio != 1 && inicio != 2) {
-            alert("valor incorrecto")
-        break;
-    }
+            while (inicio != 1 && inicio != 2) {
+                alert("valor incorrecto")
+            break;
+        }
     //break final
-    seguir = prompt("¿Quiere salir? s / n");
-        while (seguir != "s" && seguir != 'n') {
-            seguir = prompt("¿Quiere salir? s / n");
-    }
+        seguir = prompt("¿Quiere salir? s / n");
+            while (seguir != "s" && seguir != 'n') {
+                seguir = prompt("¿Quiere salir? s / n");
+        }
+}
+    
+}
+
+function bienvenido () {
+    alert("Bienvenida/o a la calculadora de Paypal (uso para mayores de 18 años)")
 }
